8482cd8c2d1df71cd9a88a5dc252ef4a90e2d82a
m.rezaei <m.rezaei@M-REZAEI>
2021-10-20 09:26:14 +0330

    change view and migrations togehter

---
+++ src/ScheduleManagement/Infrastructures/ScheduleManagement.Persistence.EF/Migrations/20211020055115_create_bookingdate_bookingtime_view.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ScheduleManagement.Persistence.EF.Migrations
+{
+    public partial class create_bookingdate_bookingtime_view : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+				               CREATE VIEW vwBookingDateAndBookingTime AS
+				                    SELECT bd.DateOfBooking,
+					                       bt.StartedBookingTime,
+					                       bt.EndedBookingTime,
+					                       us.FirstName,
+					                       us.LastName,
+					                       us.UserType,
+					                       us.Email,
+					                       us.SubjectId
+					                FROM [InterviewApplication].dbo.BookingDates AS bd
+					                LEFT JOIN [InterviewApplication].dbo.BookingTimes AS bt ON bd.Id=bt.BookingDateId
+					                LEFT JOIN [InterviewApplication].dbo.AspNetUsers AS us ON us.SubjectId = bt.SubjectId
+                					WHERE bt.IsDeleted <> 1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+drop view vwBookingDateAndBookingTime;
+");
+        }
+    }
+}
